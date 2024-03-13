27496a97c4922fcfd29e580db6f3bb03838344f3
Melomey <samuelmelomey@gmail.com>
2024-03-13 16:19:53 +0000

    installed dotenv and cors

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import todosRoutes from "./routes/todos.routes.js";
+
 
 //Create express app
 const app = express();
 
-// Define routes
-app.get('/', (req, res) => {
-    // console.log(req.query, req.headers);
-    res.send("Welcome!");
-});
+// create express app
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/ping', (req, res) => {
-    // console.log(req.query, req.headers);
-    res.send("ping pong");
-})
-
-app.get('/file', (req, res) => {
-    console.log(process.cwd());
-    res.sendFile(process.cwd() + "/index.html");
-})
-
+// Use routes
+app.use(todosRoutes);
 
 //Listen for incoming request
 app.listen(4000, () => {
